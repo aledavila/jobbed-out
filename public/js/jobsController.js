c1c99a68f68084cd35ec63e0de802729ec904e8f
Ryan Waits <ryan.waits@gmail.com>
2016-02-19 12:23:48 -0600

    add angular ajax call to our servers api to retrieve jobs

--- public/js/jobsController.js
+++ public/js/jobsController.js
@@ -2,11 +2,21 @@
 
 	var app = angular.module('JobbedOut');
 
-	app.controller('JobsController', function() {
-		this.title = "Junior Developer";
-		this.company = "General Assembly";
-		this.createdAt = "10:35am Friday, February 19th";
-		return this;
+	app.controller('JobsController', function($http) {
+		// this.title = "Junior Developer";
+		// this.company = "General Assembly";
+		// this.createdAt = "10:35am Friday, February 19th";
+		// return this;
+
+		var self = this;
+
+		$http({
+			method: 'GET',
+			url: '/api/v1/jobs'
+		})
+		.then(function success(response) {
+			console.log('success', response.data.docs);
+		});
 	});
 
 }());
