2977283a9cf5a6b15d9ece39c5a8a0641c55b2fc
Ryan Waits <ryan.waits@gmail.com>
2016-02-19 12:11:34 -0600

    add more data to job controller

--- public/js/jobsController.js
+++ public/js/jobsController.js
@@ -4,6 +4,8 @@
 
 	app.controller('JobsController', function() {
 		this.title = "Junior Developer";
+		this.company = "General Assembly";
+		this.createdAt = "10:35am Friday, February 19th";
 		return this;
 	});
 
