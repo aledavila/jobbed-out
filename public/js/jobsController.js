50a364d5fec7f56e4aac3ae8b1dae210be3ac9e8
Ryan Waits <ryan.waits@gmail.com>
2016-02-19 13:47:56 -0600

    add nextPage function for paginating through jobs

--- public/js/jobsController.js
+++ public/js/jobsController.js
@@ -3,20 +3,42 @@
 	var app = angular.module('JobbedOut');
 
 	app.controller('JobsController', function($http) {
-		// this.title = "Junior Developer";
-		// this.company = "General Assembly";
-		// this.createdAt = "10:35am Friday, February 19th";
-		// return this;
-
+		
+		// set a variable to this in order to maintain scope
 		var self = this;
 
+		// use $http to get request on our servers job api to retrieve jobs
 		$http({
 			method: 'GET',
 			url: '/api/v1/jobs'
 		})
 		.then(function success(response) {
-			console.log('success', response.data.docs);
+			self.jobs = response.data.docs;
 		});
+
+		this.currentPage = 1;
+		this.pageCount = 3;
+
+		this.nextPage = function() {
+			if (this.currentPage < this.pageCount) {
+				this.currentPage++;
+				$http({
+					method: 'GET',
+					url: '/api/v1/jobs?page=' + this.currentPage
+				})
+				.then(function success(response) {
+					self.jobs = response.data.docs;
+				});
+			} else if (this.currentPage === this.pageCount) {
+				this.currentPage = 1;
+			};
+		};
+
+		// $('#load-more').on('click', function() {
+		// 	// make an http request to load additional pages of jobs
+		// })
+
+
 	});
 
 }());
